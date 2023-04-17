e6312099bf7180c1ad4e0b48ecb10f4f2f8583f4
Ladis Nelissen <ladisnelissen@hotmail.com>
2023-04-17 13:19:56 +0200

    feat: change db connection method

--- php/signup.php
+++ php/signup.php
@@ -1,33 +1,30 @@
 <?php
 
-    if(!empty($_POST)){
-		
-        $email = $_POST["email"];
+if(!empty($_POST)){
 
-		$options = [
-			'cost' => 12,
-		];
-		$password = password_hash($_POST["password"], PASSWORD_DEFAULT, $options);
+    $username = $_POST["username"];
 
-
-		try{
-			$conn = Db::getInstance();
-			$statement = $conn->prepare("INSERT INTO users (email, password) VALUES (:email, :password)");
-			$statement->bindValue(":email", $email); //SQL injection protection
-			$statement->bindValue(":password", $password);
-			$statement->execute();
-			header("Location: login.php");
-
-		}
-		catch(Throwable $e){
-			$error = $e->getMessage();
-		}
+    $options = [
+        'cost' => 12,
+    ];
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT, $options);
 
 
+    try{
+        $conn = Db::getInstance();
+        $statement = $conn->prepare("INSERT INTO users (username, password) VALUES (:username, :password)");
+        $statement->bindValue(":username", $username); //SQL injection protection
+        $statement->bindValue(":password", $password);
+        $statement->execute();
+        header("Location: login.php");
 
     }
-
+    catch(Throwable $e){
+        $error = $e->getMessage();
+    }
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -36,34 +33,34 @@
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<div class="netflixLogin">
-		<div class="form form--login">
-			<form action="" method="post">
-				<h2 form__title>Sign up</h2>
+    <div class="netflixLogin">
+        <div class="form form--login">
+            <form action="" method="post">
+                <h2 form__title>Sign up</h2>
 
-				<?php if( isset($error) ) : ?>
-					<div class="form__error">
-						<p>
-							Sorry, we can't log you in with that email address and password. Can you try again?
-						</p>
-					</div>
-				<?php endif; ?>
+                <?php if( isset($error) ) : ?>
+                    <div class="form__error">
+                        <p>
+                            Sorry, we can't log you in with that email address and password. Can you try again?
+                        </p>
+                    </div>
+                <?php endif; ?>
 
-				<div class="form__field">
-					<label for="Email">Email</label>
-					<input type="text" name="email">
-				</div>
-				<div class="form__field">
-					<label for="Password">Password</label>
-					<input type="password" name="password">
-				</div>
+                <div class="form__field">
+                    <label for="Username">Username</label>
+                    <input type="text" name="username">
+                </div>
+                <div class="form__field">
+                    <label for="Password">Password</label>
+                    <input type="password" name="password">
+                </div>
 
-				<div class="form__field">
-					<input type="submit" value="Sign up" class="btn btn--primary">	
-					<!-- <input type="checkbox" id="rememberMe"><label for="rememberMe" class="label__inline">Remember me</label> -->
-				</div>
-			</form>
-		</div>
-	</div>
+                <div class="form__field">
+                    <input type="submit" value="Sign up" class="btn btn--primary">    
+                    <!-- <input type="checkbox" id="rememberMe"><label for="rememberMe" class="label__inline">Remember me</label> -->
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
