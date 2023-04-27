d6aa077e3207e1cc66f78d5047b32c9662ccb440
ArneSamson <arnesamson007@gmail.com>
2023-04-27 11:27:08 +0200

    feat: added more emailverified, profilpicture, and email getters and setters. Also made save funtion and getbyid and getbyemail

--- classes/User.php
+++ classes/User.php
@@ -3,9 +3,10 @@
 class User {
     private $id;
     private string $username;
+    private string $email;
+    private bool $emailVerified;
     private string $password;
-    private string $profilepicture;
-    private bool $verified;
+    private string $profilepictureUrl;
 
     public function setUsername($p_susername){
         if(empty($p_susername))
@@ -14,13 +15,15 @@ class User {
             $this->username = $p_susername;
             }
     }
+
     public function setProfilePicture($p_sprofilepicture){
         if(empty($p_sprofilepicture))
             throw new Exception("Profile picture cannot be empty");
         else{
-            $this->profilepicture = $p_sprofilepicture;
+            $this->profilepictureUrl = $p_sprofilepicture;
             }
     }
+
     public function setPassword($p_spassword){
         if(empty($p_spassword))
             throw new Exception("Password cannot be empty");
@@ -28,33 +31,57 @@ class User {
             $this->password = $p_spassword;
             }
     }
-    public function setVerification($p_bverfied){
 
+    public function setEmail($p_semail){
+        if(empty($p_semail))
+            throw new Exception("Email cannot be empty");
+        else{
+            $this->email = $p_semail;
+            }
     }
+
+    public function setEmailVerified($emailVerified) {
+        $this->emailVerified = $emailVerified;
+    }
+
+    public function isEmailVerified() {
+        return $this->emailVerified;
+    }
+
     public function setId(){
         $this->id = "placeholder";
     }
 
+    public function getId() {
+        return $this->id;
+    }
 
+    public function getUsername() {
+        return $this->username;
+    }
 
+    public function getEmail() {
+        return $this->email;
+    }
 
+    public function save(){
+        $conn = Db::getInstance();
+        $statement = $conn->prepare("INSERT INTO users (username, email, password, profilepictureUrl) VALUES (:username, :email, :password, :profilepictureUrl)");
+        $statement->bindValue(":username", $this->username);
+        $statement->bindValue(":email", $this->email);
+        $statement->bindValue(":password", $this->password);
+        $statement->bindValue(":profilepictureUrl", $this->profilepictureUrl);
+        $result = $statement->execute();
+        return $result;
+    }
 
+    public static function getById($id) {
+        // code to retrieve the user from the database based on their id, including emailVerified and profilePictureUrl
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public static function getByEmail($email) {
+        // code to retrieve the user from the database based on their email, including emailVerified and profilePictureUrl
+    }
 
 
 
