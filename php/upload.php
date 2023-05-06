d567e1ed7dcc763573cbf30e7fc6d86883aa03d3
Ladis Nelissen <ladisnelissen@hotmail.com>
2023-05-06 12:47:55 +0200

    style: change form layout

--- php/upload.php
+++ php/upload.php
@@ -114,87 +114,84 @@
       <!-- <?php include_once("../inc/functions.inc.php"); ?>  -->
       <!-- This is the nav bar -->
     
-    
-      <form class="uploadform" enctype="multipart/form-data" method="POST">
-        <h2>Upload a new prompt</h2>
-        <?php if (isset($error)) : ?> <!-- if error message is set -->
-          <p class="errormessage"><?php echo $error ?></p> <!-- display error message -->
-        <?php endif; ?>
-    
-        <label for="title">Title</label><br>
-        <input class="inputfield" type="text" id="title" name="title" required><br><br>
-    
-        <label for="description">Description</label><br>
-        <textarea style="resize: none;" class="inputfield2" id="description" name="description" required></textarea><br><br>
-    
-        <label for="description">Prompt</label><br>
-        <textarea style="resize: none;" class="inputfield2" id="prompt" name="prompt" required></textarea><br><br>
-    
-        <label for="description">Price</label><br>
-        <select class="inputfield3" id="price" name="price">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </select><br><br>
-    
-        <label for="model-type">Model</label><br>
-        <select class="inputfield3" id="model-type" name="model-type">
-          <option value="dalle">DALL-E</option>
-          <option value="midjourney">Midjourney</option>
-          <option value="lexica">Lexica</option>
-          <option value="stablediffusion">Stable Diffusion</option>
-        </select><br><br>
-    
-        <label for="image-upload">Image</label>
-        <input class="inputfield4" type="file" id="image-upload" name="image-upload" accept="image/*" required><br><br>
-    
-        <label for="title">Tags</label><br>
-        <input class="inputfield" type="text" id="tags" name="tags" required><br><br>
-    
-    
-        <p class="filter">Category:</p>
-    
-        <input type="checkbox" id="animals" name="categories[]" value="Animals">
-        <label for="animals"> Animals</label><br>
-    
-        <input type="checkbox" id="3D" name="categories[]" value="3D">
-        <label for="3D"> 3D</label><br>
-    
-        <input type="checkbox" id="space" name="categories[]" value="Space">
-        <label for="space"> Space</label><br>
-    
-        <input type="checkbox" id="game" name="categories[]" value="Game">
-        <label for="game"> Game</label><br>
-    
-        <input type="checkbox" id="car" name="categories[]" value="Car">
-        <label for="car"> Car</label><br>
-    
-        <input type="checkbox" id="nature" name="categories[]" value="Nature">
-        <label for="nature"> Nature</label><br>
-    
-        <input type="checkbox" id="portrait" name="categories[]" value="Portrait">
-        <label for="portrait"> Portrait</label><br>
-    
-        <input type="checkbox" id="anime" name="categories[]" value="Anime">
-        <label for="anime"> Anime</label><br>
-    
-        <input type="checkbox" id="interior" name="categories[]" value="Interior">
-        <label for="interior"> Interior</label><br>
-    
-        <input type="checkbox" id="realistic" name="categories[]" value="Realistic">
-        <label for="realistic"> Realistic</label><br>
-    
-        <input type="checkbox" id="geek" name="categories[]" value="Geek">
-        <label for="geek"> Geek</label><br>
-    
-        <input type="checkbox" id="building" name="categories[]" value="Building">
-        <label for="building"> Building</label><br>
-    
-        <input class="submitbtn" type="submit" value="Submit">
-      </form>
-    
+      <div class="form-container">
+<form class="uploadform" enctype="multipart/form-data" method="POST">
+  <h2>Upload a new prompt</h2>
+  <?php if (isset($error)) : ?> <!-- if error message is set -->
+    <p class="errormessage"><?php echo $error ?></p> <!-- display error message -->
+  <?php endif; ?>
+
+  <fieldset>
+    <legend>Basic Information</legend>
+
+    <label for="title">Title:</label>
+    <input type="text" id="title" name="title" required>
+
+    <label for="description">Description:</label>
+    <textarea style="resize: none;" id="description" name="description" required></textarea>
+
+    <label for="prompt">Prompt:</label>
+    <textarea style="resize: none;" id="prompt" name="prompt" required></textarea>
+
+    <label for="price">Price:</label>
+    <select id="price" name="price">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+
+    <label for="model-type">Model:</label>
+    <select id="model-type" name="model-type">
+      <option value="dalle">DALL-E</option>
+      <option value="midjourney">Midjourney</option>
+      <option value="lexica">Lexica</option>
+      <option value="stablediffusion">Stable Diffusion</option>
+    </select>
+  </fieldset>
+
+  <fieldset>
+    <legend>Image and Tags</legend>
+
+    <label for="image-upload">Image:</label>
+    <input type="file" id="image-upload" name="image-upload" accept="image/*" required>
+
+    <label for="tags">Tags:</label>
+    <input type="text" id="tags" name="tags" required>
+  </fieldset>
+
+  <fieldset>
+    <legend>Category</legend>
+
+    <input type="checkbox" id="animals" name="categories[]" value="Animals">
+    <label for="animals" class="checkbox-label">Animals</label><br>
+
+    <input type="checkbox" id="3D" name="categories[]" value="3D">
+    <label for="3D" class="checkbox-label">3D</label><br>
+
+    <!-- Add more checkboxes here -->
+
+    <!-- Example additional checkboxes -->
+    <input type="checkbox" id="space" name="categories[]" value="Space">
+    <label for="space" class="checkbox-label">Space</label><br>
+
+    <input type="checkbox" id="game" name="categories[]" value="Game">
+    <label for="game" class="checkbox-label">Game</label><br>
+
+    <input type="checkbox" id="car" name="categories[]" value="Car">
+    <label for="car" class="checkbox-label">Car</label><br>
+
+    <input type="checkbox" id="nature" name="categories[]" value="Nature">
+    <label for="nature" class="checkbox-label">Nature</label><br>
+
+    <input type="checkbox" id="portrait" name="categories[]" value="Portrait">
+    <label for="portrait" class="checkbox-label">Portrait</label><br>
+  </fieldset>
+
+  <input class="submitbtn" type="submit" value="Submit">
+</form>
+</div>
     
       <!-- <?php include_once("../inc/foot.inc.php"); ?> This is the footer -->
     </body>
