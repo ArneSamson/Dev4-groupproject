d8a9523968a2d63c317842b11169838cb7f4ce47
ArneSamson <arnesamson007@gmail.com>
2023-05-11 12:16:09 +0200

    feat: made input fields XSS save

--- php/login.php
+++ php/login.php
@@ -4,8 +4,10 @@ include_once("../inc/functions.inc.php");
 
 // Check form submit
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+
+    $username = htmlspecialchars($_POST["username"], ENT_QUOTES, 'UTF-8');
+    $password = htmlspecialchars($_POST["password"], ENT_QUOTES, 'UTF-8');
+
 
     $loginResult = canLogIn($username, $password);
     
--- php/signup.php
+++ php/signup.php
@@ -3,9 +3,9 @@
 include_once(__DIR__ . '/bootstrap.php');
 
 if (!empty($_POST)) {
-    $username = $_POST["username"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
+    $username = htmlspecialchars($_POST["username"], ENT_QUOTES, 'UTF-8');
+    $email = htmlspecialchars($_POST["email"], ENT_QUOTES, 'UTF-8');
+    $password = htmlspecialchars($_POST["password"], ENT_QUOTES, 'UTF-8');
 
     try {
         // Create a new user object
