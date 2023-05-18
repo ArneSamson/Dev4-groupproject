d0ae6c28bc7b02eb84771554f96b1bb47a9a69e1
ArneSamson <arnesamson007@gmail.com>
2023-05-18 16:33:24 +0200

    test: trying to get edit profile button only to show when the session id and the user_id from the page is the same

--- php/navbar.php
+++ php/navbar.php
@@ -32,9 +32,14 @@ $searchQuery = isset($_GET['search']) ? htmlspecialchars($_GET['search']) : '';
     <div class="navbar__buttons">
         <div class="navbar__button--credit">Credits: 0</div>
         <a href="<?php echo strpos($_SERVER['REQUEST_URI'], 'php/') !== false ? '../php/profile.php?user_id='.$user_id : 'php/profile.php?user_id='.$user_id; ?>" class="navbar__button">Profile</a>
+      
+        <!-- <?php echo $_GET['user_id']?>
+        <?php echo $user_id?> -->
+        
         <?php if (basename($_SERVER['PHP_SELF']) === 'profile.php') : ?>
             <a href="<?php echo strpos($_SERVER['REQUEST_URI'], 'php/') !== false ? 'profileEdit.php' : 'php/profileEdit.php'; ?>" class="navbar__button">Edit Profile</a>
         <?php endif; ?>
+      
         <?php if ($user_role === "admin") : ?>
             <a href="<?php echo strpos($_SERVER['REQUEST_URI'], 'php/') !== false ? 'roles.php' : 'php/roles.php'; ?>" class="navbar__button">Roles</a>
         <?php endif; ?>
