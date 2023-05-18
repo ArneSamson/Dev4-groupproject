e5631f3616707971c90654b6f231fa2c0208705b
ArneSamson <arnesamson007@gmail.com>
2023-05-18 15:40:38 +0200

    feat: when you're on the profile page a button is added to the navbar to be able to go to the profile editing page

--- php/navbar.php
+++ php/navbar.php
@@ -32,6 +32,9 @@ $searchQuery = isset($_GET['search']) ? htmlspecialchars($_GET['search']) : '';
     <div class="navbar__buttons">
         <div class="navbar__button--credit">Credits: 0</div>
         <a href="<?php echo strpos($_SERVER['REQUEST_URI'], 'php/') !== false ? '../php/profile.php?user_id='.$user_id : 'php/profile.php?user_id='.$user_id; ?>" class="navbar__button">Profile</a>
+        <?php if (basename($_SERVER['PHP_SELF']) === 'profile.php') : ?>
+            <a href="<?php echo strpos($_SERVER['REQUEST_URI'], 'php/') !== false ? 'profileEdit.php' : 'php/profileEdit.php'; ?>" class="navbar__button">Edit Profile</a>
+        <?php endif; ?>
         <?php if ($user_role === "admin") : ?>
             <a href="<?php echo strpos($_SERVER['REQUEST_URI'], 'php/') !== false ? 'roles.php' : 'php/roles.php'; ?>" class="navbar__button">Roles</a>
         <?php endif; ?>
@@ -41,3 +44,4 @@ $searchQuery = isset($_GET['search']) ? htmlspecialchars($_GET['search']) : '';
         <a href="?logout=true" class="navbar__button navbar__button--logout">Log out</a>
     </div>
 </nav>
+
