19176cfb6aa2e2b4cb0d8c7afb054e69664b1735
ArneSamson <arnesamson007@gmail.com>
2023-05-26 11:35:08 +0200

    fix: removed SQL from front-end in profileEdit page, moved updateUser function to User class

--- classes/User.php
+++ classes/User.php
@@ -125,6 +125,21 @@ class User {
         $result = $statement->fetch(PDO::FETCH_ASSOC);
         return $result;
     }
+
+    public static function getProfilePictureUrlById($id) {
+        $conn = Db::getInstance();
+        $statement = $conn->prepare("SELECT imagepath FROM users WHERE id = :id");
+        $statement->bindValue(":id", $id);
+        $statement->execute();
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+    
+        if ($result) {
+            return $result['imagepath'];
+        } else {
+            return null; // Return null if the user with the specified ID is not found
+        }
+    }
+    
     
     public function register() {
         $verificationCode = bin2hex(random_bytes(32));
@@ -280,5 +295,59 @@ class User {
 
     public static function isLoggedIn() {
         return isset($_SESSION['userId']);
+    }
+
+    public function updateUser($password){
+        
+        $fileName = htmlspecialchars($_FILES["profile_picture"]["name"]);
+        $fileTempName = $_FILES["profile_picture"]["tmp_name"];
+        $fileSize = $_FILES["profile_picture"]["size"];
+        $fileError = $_FILES["profile_picture"]["error"];
+    
+        if ($fileError !== UPLOAD_ERR_OK) {
+            $message = "Upload failed with error code $fileError.";
+            exit;
+        }
+        
+        // Check file size
+        if ($fileSize > 1000000) {
+            $message = "File is too big"; // set error message
+            header("Location: ../php/profile.php?error=" . urlencode($message)); // redirect to edit account page
+            exit;
+        }
+    
+        // Check file name for invalid characters
+        if (!preg_match('/^[a-zA-Z0-9_]+\.[a-zA-Z0-9]{3,4}$/', $fileName)) {
+            $message = "File name is not correct"; // set error message
+            header("Location: ../php/profile.php?error=" . urlencode($message)); // redirect to edit account page
+            exit;
+        }
+    
+        // Save uploaded file to disk
+        $uploadsDir = "..\\media\\";
+        $fileName = basename($fileTempName);
+        $filePath = $uploadsDir . $fileName;
+    
+        if (!move_uploaded_file($fileTempName, $filePath)) {
+            $message = "Failed to move uploaded file.";
+            exit;
+        }
+    
+        try {
+            $conn = Db::getInstance();
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+            $statement = $conn->prepare("UPDATE users SET username = :username, email = :email, password = :password, role = :role, imagepath = :imagepath WHERE id = :id");
+            $statement->bindValue(":username", $_POST['username']);
+            $statement->bindValue(":email", $_POST['email']);
+            $statement->bindValue(":password", $hash);
+            $statement->bindValue(":role", $_SESSION['role']);
+            $statement->bindValue(":imagepath", $filePath);
+            $statement->bindValue(":id", $_SESSION['user_id']);
+            $statement->execute();
+            return true;
+        } catch (Throwable $e) {
+            $error = $e->getMessage();
+            return false;
+        }
     }
 }
